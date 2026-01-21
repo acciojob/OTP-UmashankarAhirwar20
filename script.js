1f4b4240fb05e4aa34422be45dc67a3f0efaa11c
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2026-01-21 12:24:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+codes.forEach((code, index) => {
+  code.addEventListener('keydown', (e) => {
+    if (e.key >= '0' && e.key <= '9') {
+      code.value = '';
+      setTimeout(() => {
+        if (index < codes.length - 1) {
+          codes[index + 1].focus();
+        }
+      }, 10);
+    }
+
+    // If backspace is pressed
+    if (e.key === 'Backspace') {
+      code.value = '';
+      setTimeout(() => {
+        if (index > 0) {
+          codes[index - 1].focus();
+        }
+      }, 10);
+    }
+  });
+});
